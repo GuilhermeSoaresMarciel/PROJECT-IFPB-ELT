868329e0ae2f3f4975522490d7012edb3a880c24
Guilherme Soares <guilhermesoaresmarciel.dev@gmail.com>
2026-08-15 15:38:39 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h2>Instituto Federal da Paraíba</h2>
+
+        <h1>Portfólio de Eletromecânica</h1>
+
+        <h3>Campos - Itabaiana</h3>
+      </header>
+
+      <main>
+        <h1>
+          Minha jornada Eletromecânica
+          <br />
+          IFPB - Itabaiana.
+        </h1>
+
+        <p>
+          Estudante do curso técnico em
+          <br />
+          Eletromecânica pelo IFPB – Campus
+          <br />
+          Itabaiana, apaixonado por tecnologia
+          <br />e por inovações que conectam
+          <br />
+          eletricidade e mecânica para
+          <br />
+          transformar ideias em realidade.
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default App;
