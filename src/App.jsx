6b0cd22eb138351a4b068f3825b2cf8bd66675bd
Guilherme Soares <guilhermesoaresmarciel.dev@gmail.com>
2026-08-16 13:52:41 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,13 @@
-import "./App.css";
-import profile from "./assets/profile.gif";
+import TaskHeader from "./components/TaskHeader.jsx";
+import TaskMain from "./components/TaskMain.jsx";
+import TaskFooter from "./components/TaskFooter.jsx";
 
 function App() {
   return (
     <>
-      <header>
-        <h2>Instituto Federal da Paraíba</h2>
-
-        <h1>Portfólio de Eletromecânica</h1>
-
-        <h3>Campos - Itabaiana</h3>
-      </header>
-
-      <main>
-        <div id="Inicio">
-          <img className="Profile" src={profile} />
-
-          <h1>
-            Minha jornada: Eletromecânica
-            <br />
-            IFPB - Itabaiana.
-          </h1>
-
-          <p>
-            Estudante do curso técnico em Eletromecânica pelo IFPB – Campus
-            Itabaiana, apaixonado por tecnologia e por inovações que conectam
-            eletricidade e mecânica para transformar ideias em realidade.
-          </p>
-        </div>
-      </main>
-
-      <footer>
-        <h6>
-          Developed by: <u>Guilherme Soares Marciel</u>
-        </h6>
-      </footer>
+      <TaskHeader />
+      <TaskMain />
+      <TaskFooter />
     </>
   );
 }
